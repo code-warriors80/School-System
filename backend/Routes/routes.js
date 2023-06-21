e818c63facdd78f14a7398ee793704958d38822a
youngsavage-codes <117549855+youngsavage-codes@users.noreply.github.com>
2023-06-21 12:41:25 +0100

    update

--- backend/Routes/routes.js
+++ backend/Routes/routes.js
@@ -1,22 +1,23 @@
 const express = require('express')
 const router = express.Router()
+const controllers = require('../controllers/staffController')
 
 // STAFF ROUTES
 
 // GET ALL STAFFS
-router.get('/staff', () => {})
+router.get('/staff', controllers.getStaffs)
 
 // GET SPECIFIC STAFF
-router.get('/staff/:id', () => {})
+router.get('/staff/:id', controllers.getStaff)
 
 // ADD NEW STAFF
-router.post('/addStaff', () => {})
+router.post('/addStaff', controllers.addStaff)
 
 // UPDATE SPECIFIC STAFF
 router.patch('/:id/updateStaff', () => {})
 
 // DELETE SPECIFIC STAFF
-router.delete('/:id/deleteStaff', () => {})
+router.delete('/:id/deleteStaff', controllers.deleteStaff)
 
 
 module.exports = router
