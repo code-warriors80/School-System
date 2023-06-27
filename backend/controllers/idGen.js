3005889edae3f20a09012c1fc8f8f1d957505a40
adah <joshuaadah6@gmail.com>
2023-06-26 22:26:33 -0700

    completed the add staff controller

--- backend/controllers/idGen.js
+++ backend/controllers/idGen.js
@@ -1,17 +1,16 @@
 const idGenerator = (len) => {
-    if (len === 0) {
+    len += 1
+    if (len <= 0) {
         const stfId = 'STF-00' + 1
         return stfId
     } else if (len > 0 && len <= 9) {
-        console.log('less than 9')
-        const stfId = 'STF-0' + len
+        const stfId = 'STF-00' + len
         return stfId
     } else if (len > 9 && len < 100) {
-        console.log('less than 100')
-        const stfId = 'STF-0' + len
+        const stfId = 'STF-0' + len + 1
         return stfId
     } else {
-        const stfId = 'STF-' + len
+        const stfId = 'STF-' + len + 1
         return stfId
     }
     const stfId = 'STF-' + len
--- backend/controllers/staffController.js
+++ backend/controllers/staffController.js
@@ -6,7 +6,7 @@ const { default: mongoose } = require('mongoose')
 
 // GET ALL STAFF
 const getStaffs = async (req, res) => {
-    const staffs = await Staff.find()
+    const staffs = await Staff.find().select('-password')
 
     res.status(200).json(staffs)
 }
@@ -16,15 +16,14 @@ const getStaffs = async (req, res) => {
 const getStaff = async (req, res) => {
     const { id } = req.params
 
-    if(!mongoose.Types.ObjectId.isValid(id))
-    {
-        return res.status(404).json({error: 'No Such Staff'})
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'No Such Staff' })
     }
 
-    const staff = await Staff.findById(id)
+    const staff = await Staff.findById(id).select('-password')
 
-    if(!staff) {
-        return res.status(404).json({error: 'No Staff Found'})
+    if (!staff) {
+        return res.status(404).json({ error: 'No Staff Found' })
     }
     res.status(200).json(staff)
 }
@@ -33,65 +32,72 @@ const getStaff = async (req, res) => {
 
 // ADD NEW STAFF
 const addStaff = async (req, res) => {
-    const { title, firstname, lastname, surname, email, gender,  contact, address, city, state, position, role } = req.body
+    const { title, firstname, lastname, surname, email, gender, contact, address, city, state, position, role } = req.body
+
+    const staffList = await Staff.find().lean()
+    const stLength = staffList.length
 
     const password = 'Jethro10'
     const status = 'Active'
-    const staffId = idGen(0)
+    const staffId = idGen(stLength)
+
     let emptyField = []
 
-    console.log(staffId)
+    console.log(req.body)
 
     // confirm data
-    if(!firstname)
-    {
+    if (!firstname) {
         emptyField.push('firstname')
     }
-    if(!surname)
-    {
+    if (!surname) {
         emptyField.push('lastname')
     }
-    if(!gender)
-    {
+    if (!gender) {
         emptyField.push('gender')
     }
-    if(!contact)
-    {
+    if (!contact) {
         emptyField.push('contact')
     }
-    if(!position)
-    {
+    if (!position) {
         emptyField.push('position')
     }
-    if(!role)
-    {
+    if (!role) {
         emptyField.push('role')
     }
-    if(!password)
-    {
+    if (!password) {
         emptyField.push('password')
     }
-
-    if(emptyField.length > 0)
-    {
-        return res.status(400).json({error: 'Please fill in all fields'})
+    if (!address) {
+        emptyField.push('address')
+    }
+    console.log(emptyField)
+    if (emptyField.length > 0) {
+        return res.status(400).json({ error: 'Please fill in all fields' })
     }
 
-    const staff = Staff.findOne({ staffId }).exec()
-    
-    if (staff) {
-        res.json({ message: 'staff with id already exist' })
+    const findStaff = await Staff.findOne({ staffId }).exec()
+    if (findStaff) {
+        return res.json({ message: 'staff with id already exist' })
     }
     try {
         // hash the password with bcrypt
         const hashedPwd = await bcrypt.hash(password, 10)
 
-        const newStaff = await staff.create({
-            title, firstname, lastname, surname, email, gender,  contact, address, city, state, position, role, staffId, "password": hashedPwd, status
+        const newStaff = await Staff.create({
+            title, firstname, lastname,
+            surname, email, gender,
+            contact, address, city,
+            state, position, role,
+            staffId, "password": hashedPwd,
+            status
         })
+        if (newStaff) {
+            return res.json({ message: `new staff ${newStaff.firstname} created ` })
+        } else {
+            return res.status(400).json({ message: 'Invalid user data recieved' })
+        }
 
-        res.json({ message: `new staff ${newStaff.firstname} created ` })
-        
+
     } catch (error) {
         console.log(error)
     }
@@ -100,17 +106,16 @@ const addStaff = async (req, res) => {
 
 // UPDATE SINGLE STAFF
 const updateStaff = async (req, res) => {
-    const {id} = req.params
+    const { id } = req.params
 
-    if(!mongoose.Types.ObjectId.isValid(id))
-    {
-        return res.status(404).json({error: 'No Such Staff'})
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'No Such Staff' })
     }
 
     const staff = await Staff.findByIdAndUpdate(id)
 
-    if(!staff) {
-        return res.status(404).json({error: 'No Staff Found'})
+    if (!staff) {
+        return res.status(404).json({ error: 'No Staff Found' })
     }
 
     res.status(200).json(staff)
@@ -122,19 +127,18 @@ const updateStaff = async (req, res) => {
 const deleteStaff = async (req, res) => {
     const { id } = req.params
 
-    if(!mongoose.Types.ObjectId.isValid(id))
-    {
-        return res.status(404).json({error: 'No Such Staff'})
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'No Such Staff' })
     }
 
-    const staff = await Staff.findOneAndDelete({_id: id})
-    
-    if(!staff) {
-        return res.status(404).json({error: 'No Staff Found'})
+    const staff = await Staff.findOneAndDelete({ _id: id })
+
+    if (!staff) {
+        return res.status(404).json({ error: 'No Staff Found' })
     }
 
     res.status(200).json(staff)
 }
 // DELETE SINGLE STAFF
 
-module.exports = {getStaffs, getStaff, addStaff, updateStaff ,deleteStaff}
+module.exports = { getStaffs, getStaff, addStaff, updateStaff, deleteStaff }
